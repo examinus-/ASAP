7f22a117f2b58449fd987d2796f4617e6ebc8a78
David Simões <davidjorgevsimoes@gmail.com>
2015-01-27 19:18:50 +0100

    cleaned unnecessary files from repo

--- pack-dist.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm asap.tar.gz
-tar zcvf asap.tar.gz dist input stanford-models opennlp-models *word-list.txt *.sh outputs_folder.list correlation*
-
--- pack-sendto-kenobi-and-run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-./pack-dist.sh
-
-#scp asap.tar.gz david@kenobi.dei.uc.pt:asap.tar.gz
-rsync -vahz --progress -rsh=ssh asap.tar.gz david@kenobi.dei.uc.pt:asap.tar.gz
-ssh -l david kenobi.dei.uc.pt unpack-asap-and-run.sh
-
