1c54c9991ae57318f6822a647e70381f6cc66363
David Simões <davidjorgevsimoes@gmail.com>
2015-01-30 02:58:30 +0100

    New Output Format definition

---
+++ src/asap/OutputFormat.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asap;
+
+/**
+ *
+ * @author David Jorge Vieira Simões (a21210644@alunos.isec.pt) AKA examinus
+ */
+public enum OutputFormat {
+    
+        UNKNOWN,
+        SEMEVAL2014_TASK1,
+        SEMEVAL2014_TASK1_WITH_ENTAILMENT,
+        SEMEVAL2014_TASK10,
+        SEMEVAL2015_TASK2a,
+        SEMEVAL2015_TASK2a_WITH_PRECISION,
+}
